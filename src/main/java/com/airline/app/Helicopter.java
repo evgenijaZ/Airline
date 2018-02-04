cc339d7f139f1ad7b65a5efb3c5f23aa5637017b
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 15:35:05 +0200

    methods are implemented

--- src/main/java/com/airline/app/Helicopter.java
+++ src/main/java/com/airline/app/Helicopter.java
@@ -8,10 +8,15 @@ public class Helicopter extends Aircraft {
      * the main rotor diameter in meters
      */
     double rotorDiameter_m;
+    /**
+     * speed of the helicopter blades in meters per second
+     */
+    double velocity_mps;
 
     Helicopter(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km, double rotorDiameter_m) {
         super(modelName, capacity, carryingCapacity_kg, flightRange_km);
         this.rotorDiameter_m = rotorDiameter_m;
+        this.velocity_mps = 0;
     }
 
     /**
@@ -19,7 +24,9 @@ public class Helicopter extends Aircraft {
      */
     @Override
     public void goUp() {
-
+        rotate();
+        isFlying = true;
+        System.out.println("The helicopter " + modelName + " goes up.");
     }
 
     /**
@@ -27,6 +34,24 @@ public class Helicopter extends Aircraft {
      */
     @Override
     public void goDown() {
+        this.velocity_mps = 0;
+        isFlying = false;
+        System.out.println("The helicopter " + modelName + " goes down.");
+    }
 
+    /**
+     * rotate the blades of a helicopter
+     */
+    public void rotate() {
+        velocity_mps = (rotorDiameter_m / 2) * (3.14 * 190 / 30);
+        System.out.println("The helicopter rotates. Speed of the helicopter blades is " + this.velocity_mps + " mps");
+    }
+
+    public double getRotorDiameter_m() {
+        return rotorDiameter_m;
+    }
+
+    public void setRotorDiameter_m(double rotorDiameter_m) {
+        this.rotorDiameter_m = rotorDiameter_m;
     }
 }
