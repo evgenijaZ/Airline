6fc619198de42abc670c750101adfa8d5300b663
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 13:21:12 +0200

    the aircraft is made abstract, added abstract methods goUp(), goDown()

--- src/main/java/com/airline/app/Aircraft.java
+++ src/main/java/com/airline/app/Aircraft.java
@@ -3,18 +3,24 @@ package com.airline.app;
 /**
  * @author Yevheniia Zubrych on 03.02.2018.
  */
-public class Aircraft {
+public abstract class Aircraft {
     private String modelName;
     private int capacity;
     private int carryingCapacity_kg;
     private int flightRange_km;
+    private boolean isFlying;
 
     public Aircraft(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km) {
         this.modelName = modelName;
         this.capacity = capacity;
         this.carryingCapacity_kg = carryingCapacity_kg;
         this.flightRange_km = flightRange_km;
+        this.isFlying = false;
     }
+
+    public abstract void goUp();
+
+    public abstract void goDown();
 
     public String getModelName() {
         return modelName;
@@ -32,4 +38,7 @@ public class Aircraft {
         return flightRange_km;
     }
 
+    public boolean isFlying() {
+        return isFlying;
+    }
 }
