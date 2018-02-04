849a3a955e4dadb1dadd7de0a0da59bdc87d6d31
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 17:31:36 +0200

    added cruising speed, methods are implemented

--- src/main/java/com/airline/app/Airplane.java
+++ src/main/java/com/airline/app/Airplane.java
@@ -8,10 +8,15 @@ public class Airplane extends Aircraft {
      * the fuel consumption of the airplane in liters per hour
      */
     private double fuelConsumption_lph;
+    /**
+     * cruising speed of the airplane in kilometre per hour
+     */
+    private double cruisingSpeed_kmph;
 
-    public Airplane(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km, double fuelConsumption_lph) {
+    public Airplane(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km, double fuelConsumption_lph, double cruisingSpeed_kmph) {
         super(modelName, capacity, carryingCapacity_kg, flightRange_km);
         this.fuelConsumption_lph = fuelConsumption_lph;
+        this.cruisingSpeed_kmph = cruisingSpeed_kmph;
     }
 
     /**
@@ -19,7 +24,13 @@ public class Airplane extends Aircraft {
      */
     @Override
     public void goUp() {
-
+        if (this.isFlying) {
+            System.out.println("The airplane " + modelName + " is already flying.");
+            return;
+        }
+        System.out.println("The airplane " + modelName + " leaves on a runway, accelerates, takes off.");
+        isFlying = true;
+        System.out.println("The airplane " + modelName + " gained cruising speed " + this.cruisingSpeed_kmph + "km/h and goes up.");
     }
 
     /**
@@ -27,7 +38,13 @@ public class Airplane extends Aircraft {
      */
     @Override
     public void goDown() {
-
+        if (!this.isFlying) {
+            System.out.println("The airplane " + modelName + " is not in flight yet.");
+            return;
+        }
+        System.out.println("The airplane " + modelName + " lands on the runway.");
+        isFlying = false;
+        System.out.println("The airplane " + modelName + " goes down.");
     }
 
     public double getFuelConsumption_lph() {
@@ -36,5 +53,13 @@ public class Airplane extends Aircraft {
 
     public void setFuelConsumption_lph(double fuelConsumption_lph) {
         this.fuelConsumption_lph = fuelConsumption_lph;
+    }
+
+    public double getCruisingSpeed_kmph() {
+        return cruisingSpeed_kmph;
+    }
+
+    public void setCruisingSpeed_kmph(double cruisingSpeed_kmph) {
+        this.cruisingSpeed_kmph = cruisingSpeed_kmph;
     }
 }
