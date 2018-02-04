b44a1285c6069b75b841b952de0b0b63772a76f5
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 13:36:41 +0200

    added comments

--- src/main/java/com/airline/app/Aircraft.java
+++ src/main/java/com/airline/app/Aircraft.java
@@ -4,10 +4,30 @@ package com.airline.app;
  * @author Yevheniia Zubrych on 03.02.2018.
  */
 public abstract class Aircraft {
+    /**
+     * the model name of the aircraft,
+     * it is defined in the constructor
+     */
     private String modelName;
+    /**
+     * the passenger capacity of the aircraft,
+     * it is defined in the constructor
+     */
     private int capacity;
+    /**
+     * maximum carrying capacity of the aircraft in kilograms,
+     * it is defined in the constructor
+     */
     private int carryingCapacity_kg;
+    /**
+     * maximum flight range of the aircraft in kilometers,
+     * it is defined in the constructor
+     */
     private int flightRange_km;
+    /**
+     * Is there an airplane in flight,
+     * default value is false
+     */
     private boolean isFlying;
 
     public Aircraft(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km) {
@@ -18,8 +38,14 @@ public abstract class Aircraft {
         this.isFlying = false;
     }
 
+    /**
+     * abstract method of takeoff of an aircraft
+     */
     public abstract void goUp();
 
+    /**
+     * abstract method of landing an aircraft
+     */
     public abstract void goDown();
 
     public String getModelName() {
