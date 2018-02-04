4b56d08fa2cdb891c91afdb06539b8c69a39eeca
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 13:16:41 +0200

    added the basic properties of the aircraft

--- src/main/java/com/airline/app/Aircraft.java
+++ src/main/java/com/airline/app/Aircraft.java
@@ -4,4 +4,32 @@ package com.airline.app;
  * @author Yevheniia Zubrych on 03.02.2018.
  */
 public class Aircraft {
+    private String modelName;
+    private int capacity;
+    private int carryingCapacity_kg;
+    private int flightRange_km;
+
+    public Aircraft(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km) {
+        this.modelName = modelName;
+        this.capacity = capacity;
+        this.carryingCapacity_kg = carryingCapacity_kg;
+        this.flightRange_km = flightRange_km;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getCarryingCapacity_kg() {
+        return carryingCapacity_kg;
+    }
+
+    public int getFlightRange_km() {
+        return flightRange_km;
+    }
+
 }
