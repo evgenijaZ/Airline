3b2c192c772f279367ff8775a75488d27e872c5f
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 12:46:54 +0200

    added copyright comment

--- src/main/java/com/airline/app/App.java
+++ src/main/java/com/airline/app/App.java
@@ -1,5 +1,8 @@
 package com.airline.app;
-
+/**
+ * Main class of the airline application
+ * @author Yevheniia Zubrych on 03.02.2018.
+ */
 public class App {
     public static void main(String[] args){
 
