92d6c2e326e013d8d663ea22d0404516dc3e1720
evgenijaZ <evg.zubrich@gmail.com>
2018-02-04 14:54:56 +0200

    added implemented methods, an attribute rotor diameter

--- src/main/java/com/airline/app/Helicopter.java
+++ src/main/java/com/airline/app/Helicopter.java
@@ -3,5 +3,30 @@ package com.airline.app;
 /**
  * @author Yevheniia Zubrych on 04.02.2018.
  */
-public class Helicopter {
+public class Helicopter extends Aircraft {
+    /**
+     * the main rotor diameter in meters
+     */
+    double rotorDiameter_m;
+
+    Helicopter(String modelName, int capacity, int carryingCapacity_kg, int flightRange_km, double rotorDiameter_m) {
+        super(modelName, capacity, carryingCapacity_kg, flightRange_km);
+        this.rotorDiameter_m = rotorDiameter_m;
+    }
+
+    /**
+     * abstract method of takeoff of an aircraft
+     */
+    @Override
+    public void goUp() {
+
+    }
+
+    /**
+     * abstract method of landing an aircraft
+     */
+    @Override
+    public void goDown() {
+
+    }
 }
