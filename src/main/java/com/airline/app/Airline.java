f93979cc4fac405bb84c0982d10871a3149c3b6e
evgenijaZ <evg.zubrich@gmail.com>
2018-02-05 16:51:47 +0200

    added list of airplanes, method of sorting by fuel consumption, filtering by flight range

--- src/main/java/com/airline/app/Airline.java
+++ src/main/java/com/airline/app/Airline.java
@@ -1,7 +1,39 @@
 package com.airline.app;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Yevheniia Zubrych on 04.02.2018.
  */
 public class Airline {
+    private List <Airplane> airplanes;
+
+    public Airline() {
+        airplanes = new ArrayList <>();
+    }
+
+    public List <Airplane> getAirplanes() {
+        return airplanes;
+    }
+
+    public void addAirplane(Airplane airplane) {
+        airplanes.add(airplane);
+    }
+
+    public void removeAirplane(Airplane airplane) {
+        airplanes.remove(airplane);
+    }
+
+    public List <Airplane> sortAirplanesByFlightRange() {
+        airplanes.sort(Comparator.comparingInt(Aircraft::getFlightRange_km));
+        return airplanes;
+    }
+
+    public List <Airplane> filterByFuelConsumption(int minValue, int maxValue) {
+        Iterable <Airplane> filtered = airplanes.stream().filter(airplane -> airplane.getFuelConsumption_lph() >= minValue && airplane.getFuelConsumption_lph() <= maxValue).collect(Collectors.toList());
+        return (List <Airplane>) filtered;
+    }
 }
