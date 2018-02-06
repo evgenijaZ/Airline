ca9b455e78533190c8bcd794cbd5dd1f27a3f6f1
evgenijaZ <evg.zubrich@gmail.com>
2018-02-06 19:26:40 +0200

    added test for filtering by fuel consumption and calculating total capacity

--- src/test/java/com/airline/app/AirlineTest.java
+++ src/test/java/com/airline/app/AirlineTest.java
@@ -41,7 +41,50 @@ public class AirlineTest {
                 flag = false;
         }
         assertTrue(flag);
+        airplanes.add(new CargoPlane("An-124-100", 402000, 0, 12600, 800, 20000));
+        for (int i = 1; i < airplanes.size(); i++) {
+            if (airplanes.get(i - 1).getFlightRange_km() > airplanes.get(i).getFlightRange_km())
+                flag = false;
+        }
+        assertFalse(flag);
     }
-    
+    @Test
+    public void filterByFuelConsumption() throws Exception {
+        assertEquals(airline.filterByFuelConsumption(1000, -2000).size(), 0);
+        int min = 1000;
+        int max = 2000;
+        List <Airplane> airplanes = airline.filterByFuelConsumption(min, max);
+        boolean flag = true;
+        for (int i = 1; i < airplanes.size(); i++) {
+            if (min > airplanes.get(i).getFuelConsumption_lph() ||
+                    airplanes.get(i).getFuelConsumption_lph() > max)
+                flag = false;
+        }
+        assertTrue(flag);
+    }
+
+    @Test
+    public void getTotalCapacity() throws Exception {
+        assertEquals((new Airline("Empty airline")).getTotalCapacity(), 0);
+        int totalCapacity = airline.getTotalCapacity();
+        airline.addAirplane(new CargoPlane("An-124-100", 402000, 15700, 12600, 800, 20000));
+        assertEquals(totalCapacity, airline.getTotalCapacity());
+        airline.addAirplane(new PassengerPlane("Embraer E-190", 114, 51800, 4537, 1850, 852, 105));
+        assertEquals(totalCapacity + 114, airline.getTotalCapacity());
+
+    }
+
+    @Test
+    public void getTotalCarryingCapacity() throws Exception {
+        assertEquals((new Airline("Empty airline")).getTotalCarryingCapacity(), 0);
+        int totalCarryingCapacity = airline.getTotalCarryingCapacity();
+        airline.addAirplane(new CargoPlane("An-124-100", 402000, 15700, 12600, 800, 20000));
+        totalCarryingCapacity += 402000;
+        assertEquals(totalCarryingCapacity, airline.getTotalCarryingCapacity());
+        airline.addAirplane(new PassengerPlane("Embraer E-190", 114, 51800, 4537, 1850, 852, 105));
+        totalCarryingCapacity += 51800;
+        assertEquals(totalCarryingCapacity, airline.getTotalCarryingCapacity());
+
+    }
 
 }
